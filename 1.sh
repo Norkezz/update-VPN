e4ce3b59edb3fb89c372b47ef9e645fdb97d1da2
Norkezz <norkezzadm@gmail.com>
2026-04-27 04:21:28 +1000

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,2 @@
+./chmod +x admin_config_update7.py
+./admin_config_update7.py diag 'vless://6202b230-417c-4d8e-b624-0f71afa9c75d@181.214.1.30:8880?encryption=none&security=none&type=ws&host=vms.lifetime39.workers.dev&path=%2F%3Fed%3D2560%26https%3A%2F%2Ft.me%2FWangCai2%F0%9F%87%A8%F0%9F%87%B3#Key Germany #998 / keysconf.com'
